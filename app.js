d6f9131073570595a658c68895d1541e169aa6b9
swarnimbungrai <raiswornimbung@gmail.com>
2023-07-04 14:30:24 +0545

    learned about request,response and how nodemon works and git command as well

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express= require ('express')
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send("hello world")
+    res.send("hello world hello")
 });
 
 
 app.listen(4000,()=>{
     console.log("server started at ports 4000")
-});
+}); 
