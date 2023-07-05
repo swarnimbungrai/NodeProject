6622567fdf18283d1167d17fd17d8d3a1def9450
swarnimbungrai <raiswornimbung@gmail.com>
2023-07-05 15:12:06 +0545

    learned about EJS and config

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const express= require ('express')
+const express= require ('express') //importing express module(framework)
 const app = express();
 
-app.get('/',(req, res) => {
-    res.send("hello world hello")
+const ejs = require ("ejs");
+app.set("view engine", "ejs") //telling node to use ejs
+    
+const {sequelize} = require ('./model')
+
+
+app.get('/register',(req, res) => {
+   // res.send("<h1>hello i am about page! </h1>")
+   res.render('register.ejs');
+
 });
+app.get('/login',(req, res) => {
+    // res.send("<h1>hello i am about page! </h1>")
+    res.render('login.ejs');
+ 
+ });
 
 
 app.listen(4000,()=>{
     console.log("server started at ports 4000")
 }); 
+
